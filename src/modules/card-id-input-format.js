de1ea8952d0d4e2a84a7cc1ff45dac82b354250e
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-08-07 00:04:20 -0300

    limiting cardId input to 12 numbers

--- src/modules/card-id-input-format.js
+++ src/modules/card-id-input-format.js
@@ -5,6 +5,11 @@ if (cardId) {
     // Get input value and remove non-numeric characters
     let value = cardId.value.replace(/\D/g, '')
 
+    // Limit the length of the value to 12 characters
+    if (value.length > 12) {
+      value = value.slice(0, 12)
+    }
+
     // Insert hyphens every 3 digits
     value = value.replace(/(.{3})(?=.)/g, '$1-')
 
