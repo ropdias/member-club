9d9d5a32ef05c204d2ce72ac61278bd3b057d9c8
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-08-08 22:26:48 -0300

    updating more user information if valid ID

--- src/modules/load-client-info.js
+++ src/modules/load-client-info.js
@@ -3,6 +3,9 @@ import { clientFindById } from '../services/client-find-by-id.js'
 const form = document.getElementById('header-id-form')
 const input = document.getElementById('card-id')
 const userAvatar = document.getElementById('user-avatar-img')
+const userName = document.getElementById('user-name')
+const userClientSince = document.getElementById('user-client-since')
+const cardHeaderIdTag = document.getElementById('card-header-id-tag')
 
 if (form && input) {
   form.onsubmit = async (event) => {
@@ -19,6 +22,9 @@ if (form && input) {
     }
 
     userAvatar.src = `./assets/avatars/${clientData[0].imgKey}`
+    userName.innerHTML = clientData[0].name
+    userClientSince.innerHTML = `Cliente desde ${clientData[0].clientSince}`
+    cardHeaderIdTag.innerHTML = `ID: ${clientData[0].id}`
 
     console.log(clientData)
   }
