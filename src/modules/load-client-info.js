5336061e4381287133daf99a69d8d7fb69459a17
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-08-08 22:49:57 -0300

    updating history list of haircuts

--- src/modules/load-client-info.js
+++ src/modules/load-client-info.js
@@ -6,6 +6,8 @@ const userAvatar = document.getElementById('user-avatar-img')
 const userName = document.getElementById('user-name')
 const userClientSince = document.getElementById('user-client-since')
 const cardHeaderIdTag = document.getElementById('card-header-id-tag')
+const totalCuts = document.getElementById('history-header-total-cuts')
+const historyList = document.getElementById('history-list')
 
 if (form && input) {
   form.onsubmit = async (event) => {
@@ -25,6 +27,45 @@ if (form && input) {
     userName.innerHTML = clientData[0].name
     userClientSince.innerHTML = `Cliente desde ${clientData[0].clientSince}`
     cardHeaderIdTag.innerHTML = `ID: ${clientData[0].id}`
+
+    totalCuts.innerHTML = `${clientData[0].loyaltyCard.totalCuts} cortes`
+
+    // <li class="history-list-item">
+    //   <div class="history-list-item-data">
+    //     <h2>29/04/2024</h2>
+    //     <span>18:30</span>
+    //   </div>
+    //   <div class="history-list-item-check">
+    //     <img src="./assets/icons/seal-check.svg" alt="seal-check" />
+    //   </div>
+    // </li>
+
+    historyList.innerHTML = ''
+
+    clientData[0].appointmentHistory.forEach((appointment) => {
+      const item = document.createElement('li')
+
+      // Creating itemData
+      const itemData = document.createElement('div')
+      const date = document.createElement('h2')
+      const time = document.createElement('span')
+      date.innerHTML = appointment.date
+      time.innerHTML = appointment.time
+      itemData.classList.add('history-list-item-data')
+      itemData.append(date, time)
+
+      // Creating itemCheck
+      const itemCheck = document.createElement('div')
+      const sealIcon = document.createElement('img')
+      sealIcon.setAttribute('src', './assets/icons/seal-check.svg')
+      sealIcon.setAttribute('alt', 'seal-check')
+      itemCheck.classList.add('history-list-item-check')
+      itemCheck.append(sealIcon)
+
+      item.classList.add('history-list-item')
+      item.append(itemData, itemCheck)
+      historyList.appendChild(item)
+    })
 
     console.log(clientData)
   }
