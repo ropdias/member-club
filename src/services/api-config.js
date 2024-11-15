e0a662b5ddd56adf15fff23d7a7943ec3ca261b7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-11-15 01:35:30 -0300

    using db.json as default data for local storage

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,6 +1,17 @@
+import dbData from '../../db.json'
+
 export const apiConfig = {
   baseURL: 'http://localhost:3333',
 }
 
 // Configuration to choose between API and localStorage
-export const useAPI = true // Change to false to use localStorage
+export const useAPI = false // Change to false to use localStorage
+
+if (!useAPI) {
+  // Check if 'clients' is already in localStorage
+  if (!localStorage.getItem('clients')) {
+    const { clients } = dbData
+    // Set 'clients' in localStorage
+    localStorage.setItem('clients', JSON.stringify(clients))
+  }
+}
